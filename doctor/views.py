a5d9d51c499ee4499a61da55c971d63fa1d3ed89
ialimoud <alimoudineidrissou@gmail.com>
2023-01-15 16:23:06 +0000

    appointment

--- doctor/views.py
+++ doctor/views.py
@@ -1,6 +1,57 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic.base import TemplateView
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.contrib import messages
+from .models import Appointment
 # Create your views here.
 class HomeTemplateView(TemplateView):
-    template_name="index.html"
+    template_name = "index.html"
+    
+    def post(self, request):
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        email = EmailMessage(
+            subject= f"{name} from doctor family.",
+            body=message,
+            from_email=settings.EMAIL_HOST_USER,
+            to=[settings.EMAIL_HOST_USER],
+            reply_to=[email]
+        )
+        email.send()
+        return HttpResponse("Email sent successfully!")
+
+class AppointmentTemplateView(TemplateView):
+    template_name = "appointment.html"
+
+    def post(self,request):
+        fname=request.POST.get("fname")
+        lname=request.POST.get("lname")
+        email=request.POST.get("email")
+        mobile=request.POST.get("mobile")
+        message=request.POST.get("request")
+
+        appointment=Appointment.objects.create(
+            first_name=fname,
+            last_name=lname,
+            email=email,
+            phone=mobile,
+            request=message,
+        )
+        appointment.save()
+
+        messages.add_message(request,messages.SUCCESS,f"Thanks {fname} for making an appointment")
+        return HttpResponseRedirect(request.path)
+
+class ManageAppointmentTemplateView(TemplateView):
+    template_name = "manage-appointments.html"
+    login_required=True
+
+
+
+
+
+
--- project/settings.py
+++ project/settings.py
@@ -115,3 +115,11 @@ else:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#Sending Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EADDRESS')
+EMAIL_HOST_PASSWORD = config('EAPASSWORD')
+
